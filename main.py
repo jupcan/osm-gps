8f15f2251c645429470404e69e4f8686090f8384
Juan Perea <jpereac@gmail.com>
2018-09-30 01:46:49 +0200

    positionNode

--- main.py
+++ main.py
@@ -7,12 +7,23 @@ def main():
     
     nodes = [node.get('id') for node in root.findall('n:graph/n:node', ns)]
     print(belongNode(nodes,'950073335'))
+    positionNode(nodes,id)
 
 def belongNode(nodes,id):
     #input: osm node id, output: true/false
     try:
         nodes.index(id) 
     except ValueError:
+        return False
+    else:
+        return True
+
+def positionNode(nodes,id):
+    #input: osm node id, output: longitude&latitude of given node
+    try:
+        nodes.index(id) 
+    except ValueError:
+        print("Error. The node does not exist.")
         return False
     else:
         return True
