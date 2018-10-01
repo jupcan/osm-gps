ef82711ae3c57aebf8fb639e2deddc57fb0edbfb
Juan Perea <jpereac@gmail.com>
2018-10-01 18:38:46 +0200

    graph.py creation

---
+++ graph.py
@@ -0,0 +1,43 @@
+from lxml import etree
+
+class graph():
+    _path=""
+    _nodes = []
+    def __init__(self,path):
+        self._path=path
+        self._nodes=self._readFile()
+
+    def _readFile(self):
+        data = etree.parse(self._path)
+        root = data.getroot()
+        ns = {'n': 'http://graphml.graphdrawing.org/xmlns'}
+    
+        nodes = []
+        for node in root.findall('n:graph/n:node',ns):
+            id = node.get('id')
+            nodes.append((id, *(data.text for data in node if data.get('key') != 'd6')))
+        #print(nodes)
+        return nodes
+
+    def belongNode(self,id):
+        #input: osm node id, output: true/false
+        node_exists = False
+        for data in self._nodes:
+            if data[0] == id:
+                node_exists = True
+        if node_exists:
+            coordinates = (data[1],data[2])
+            return True, coordinates
+        else:
+            return False, False
+
+    def positionNode(self,id):
+        #input: osm node id, output: latitude&longitude 
+        try:
+            node_exists, coordinates = self.belongNode(id)
+            if node_exists:
+                print(coordinates)
+            else:
+                raise ValueError
+        except ValueError:
+            print("Error. The node does not exist.")
--- main.py
+++ main.py
@@ -1,41 +1,11 @@
-from lxml import etree
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+from graph import graph
 
 def main():
-    data = etree.parse('data/navalpino.graphml.xml')
-    root = data.getroot()
-    ns = {'n': 'http://graphml.graphdrawing.org/xmlns'}
-    
-    nodes = []
-    for node in root.findall('n:graph/n:node',ns):
-        id = node.get('id')
-        nodes.append((id, *(data.text for data in node if data.get('key') != 'd6')))
-    #print(nodes)
-
-    print(belongNode(nodes,'950073331')[0])
-    positionNode(nodes,'950073331')
-
-def belongNode(nodes,id):
-    #input: osm node id, output: true/false
-    node_exists = False
-    for data in nodes:
-        if data[0] == id:
-            node_exists = True
-    if node_exists:
-        coordinates = (data[1],data[2])
-        return True, coordinates
-    else:
-        return False, False
-
-def positionNode(nodes,id):
-    #input: osm node id, output: latitude&longitude 
-    try:
-        node_exists, coordinates = belongNode(nodes,id)
-        if node_exists:
-            print(coordinates)
-        else:
-            raise ValueError
-    except ValueError:
-        print("Error. The node does not exist.")
+    town1 = graph('data/navalpino.graphml.xml')
+    print(town1.belongNode('950073331')[0])
+    town1.positionNode('950073331')
 
 if __name__ == '__main__':
     main()
