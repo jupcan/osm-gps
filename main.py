e3dc83268f697b164ac32c4e3629b45e70404cf4
Juan Perea <jpereac@gmail.com>
2018-10-02 12:34:25 +0200

    check times

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 from graph import graph
+import time
 
 def main():
-    town1 = graph('data/anchuras.graphml.xml')
+    town1 = graph('data/puertollano.graphml.xml')
     print(town1.belongNode('946409139')[0])
     town1.positionNode('982621804')
-    town1.adjacentNode('4331489709')
+    town1.adjacentNode('956428288')
 
 if __name__ == '__main__':
+    start_time = time.time()
     main()
+    print("%s seconds" % (time.time() - start_time))
