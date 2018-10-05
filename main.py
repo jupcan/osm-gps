22ac38328d934872926ddc048d1ce8eefc044919
Juan Perea <jpereac@gmail.com>
2018-10-05 23:35:24 +0200

    delete comment

--- main.py
+++ main.py
@@ -20,10 +20,5 @@ def main():
     except ValueError:
         print ("Error. Not a valid input.")
     
-    """town1 = graph('data/anchuras.graphml.xml')
-    print(town1.belongNode('4331489627'))
-    town1.positionNode('4331489627')
-    town1.adjacentNode('4331489627')"""
-    
 if __name__ == '__main__':
     main()
