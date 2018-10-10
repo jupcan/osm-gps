4b7ff4fab5bb8030c2f285bae8faa30f7187136b
Samuel <samuel.gonzalez3@alu.uclm.es>
2018-10-10 20:04:42 +0200

    state.py creation
    state class

---
+++ state.py
@@ -0,0 +1,10 @@
+class state():
+    
+    _current = ""
+    _nodes = {}
+    _md5 = ""
+
+    def __init__(self, current, nodes, md5):
+        self._current = current
+        self._nodes = nodes
+        md5._md5 = md5
