6f6f7d9d64be122cf06b1b1d8c3bb8448e8cb86e
jupcan <jpereac@gmail.com>
2018-10-17 23:32:30 +0200

    minor class changes

--- treeNode.py
+++ treeNode.py
@@ -1,5 +1,4 @@
-import state from state as state
-
+import state from state
 
 class node():
 	_state
@@ -11,6 +10,6 @@ class node():
 	def __init__(self,state,cost,action,d):
 		self._state = state("","","")#current state of the problem
 		self._cost #cost from the inicial node to the current node (this one)
-		self._action 
+		self._action
 		self._d #depth of the node.
 		self._f=random.uniform(1,1000) #random value that determines the insertion order in the frontier
