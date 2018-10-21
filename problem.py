df631499dd2a990de2e3fd750ed82ec32e896e13
jupcan <jpereac@gmail.com>
2018-10-21 18:25:02 +0200

    stateSpace reading

--- problem.py
+++ problem.py
@@ -6,15 +6,16 @@ import hashlib
 class problem():
     _file = {}
     _init_state = state("", "", "")
-    _state_space = stateSpace("", "")
-    def __init__(self, path):
-        self._path = path
+    _state_space = stateSpace("")
+    def __init__(self, json):
+        self._json = json
         self._file = self._readJson()
         self._init_state = state(self._file["IntSt"]["node"], self._file["IntSt"]["listNodes"], self._file["IntSt"]["id"])
-        self._state_space = stateSpace("/".join(self._file["graphlmfile"].strip("/").split('/')[1:]), self._init_state) #not getting town folder
+        path = "/".join(self._file["graphlmfile"].strip("/").split('/')[1:]) + ".xml" #not getting town folder and adding .xml
+        self._state_space = stateSpace(path)
 
     def _readJson(self):
-        with open(self._path) as json_data:
+        with open(self._json) as json_data:
             return json.load(json_data)
 
     def isGoal(self, state):
--- stateSpace.py
+++ stateSpace.py
@@ -1,9 +1,68 @@
+from lxml import etree
+from pprint import pprint
 from state import state
 
 class stateSpace():
-    def __init__(self, path, state):
+    def __init__(self, path):
         self._path = path
-        self._states = state
+        self._keys, self._nodes, self._edges = self._readFile()
+
+    def _readFile(self):
+        data = etree.parse(self._path)
+        root = data.getroot()
+        ns = {'n': 'http://graphml.graphdrawing.org/xmlns'}
+
+        keys, nodes, edges = {}, {}, {}
+        for key in root: #get desired key values for each file
+            keys[(key.get('attr.name'), key.get('for'))] = (key.get('id'))
+        key_y = keys[('y', 'node')]
+        key_x = keys[('x', 'node')]
+        key_name = keys[('name', 'edge')]
+        key_length = keys[('length', 'edge')]
+
+        for node in root.findall('n:graph/n:node', ns):
+            data = dict((d.get('key'), d.text) for d in node)
+            values = (data.get(key_y), data[key_x])
+            nodes[node.get('id')] = values
+
+        for edge in root.findall('n:graph/n:edge', ns):
+            data = dict((d.get('key'), d.text) for d in edge)
+            values = (data.get(key_name, 'sinNombre'), data[key_length])
+            edges[(edge.get('source'), edge.get('target'))] = values
+        return keys, nodes, edges
+
+    def belongNode(self, id):
+        #input: osm node id, output: true/false
+        if id in self._nodes:
+            return True
+        else:
+            return False
+
+    def positionNode(self, id):
+        #input: osm node id, output: latitude&longitude[(y,x)]
+        try:
+            node_exists = self.belongNode(id)
+            if node_exists:
+                print([self._nodes[id]])
+            else:
+                raise ValueError
+        except ValueError:
+            print("Error. The node does not exist.")
+
+    def adjacentNode(self, id):
+        #input: osm node id, output: list of adjacent arcs
+        try:
+            node_exists = self.belongNode(id)
+            streets = {}
+            if node_exists:
+                adjacents = [key for key in self._edges.keys() if id in key[0]]
+                for data in adjacents:
+                    streets[data] = tuple(self._edges[data])
+                pprint(streets)
+            else:
+                raise ValueError
+        except ValueError:
+            print("Error. The node does not exist.")
 
     def successors(self, state):
         acc = "i'm in %s and i go to %s" %(state._current, state._nodes[0])
