f4154e836eb0c5e7c494f70f6ca7285c483ba79b
Samuel <samuel.gonzalez3@alu.uclm.es>
2018-10-26 21:20:13 +0200

    update priority queue test

--- main.py
+++ main.py
@@ -7,8 +7,12 @@ from treeNode import treeNode
 import time
 import sys
 
-"""def stressTest(f,p):
+def stressTest(f,p):
     elements = 0
+    avg = 0
+    maxim = 0
+    minim = 9999
+    
     try:
         while True:
             try:
@@ -17,16 +21,33 @@ import sys
                     node = treeNode(p._init_state, 0, "", elements)
                 else:
                     node = treeNode(newState, 0, "", elements)
+
+                start = time.time()
                 f.insert(node)
+                end = time.time()
+
+                timer = end - start
+                avg += timer
+                if(timer > maxim):
+                    maxim = timer
+                if(timer < minim):
+                    minim = timer
+                
                 elements+=1
-                #print(f._frontier[node._f]._state._md5)
+                
             except MemoryError:
                 print("full memory")
+                print('nº elements: '+elements)
+                print('min time: %.11f' % minim)
+                print('max time: %.11f' %  maxim)
+                print('avg time: %.11f' % (avg/elements))
                 break
     except KeyboardInterrupt:
-            print(min(f._frontier))
+            print('min time: %.11f' % minim)
+            print('max time: %.11f' %  maxim)
+            print('avg time: %.11f' % (avg/elements))
             return elements
-    return elements"""
+    return elements
 
 def main():
     try:
@@ -35,16 +56,22 @@ def main():
             raise ValueError
         print(filename + ".json") #print json file name
         p = problem('%s.json' % filename)
-        tn1 = treeNode(p._init_state, 0, "", 0)
+        f = frontier()
+        
+        """tn1 = treeNode(p._init_state, 0, "", 0)
         tn2 = treeNode(p._init_state, 1, "", 1)
         tn3 = treeNode(p._init_state, 2, "", 2)
-        f = frontier()
+
+        
         f.insert(tn1)
         f.insert(tn2)
         f.insert(tn3)
+
         #print(str(p._init_state))
-        while not f._frontier.empty():
-            print(f._frontier.get())
+
+        "while not f._frontier.empty():
+            print(f._frontier.get())"""
+
         #f.remove()
         #print(f._frontier)
         #print(p._init_state._md5)
@@ -53,11 +80,11 @@ def main():
         #p._state_space.successors(p._init_state)
         #print(p.isGoal(p._init_state))
 
-        """print('elements until memory error: ')
+        print('elements until memory error: ')
         start = time.time()
         print(stressTest(f,p))
         print('time: %.11f' % (time.time() - start))
-        #print(f._frontier[min(f._frontier)])"""
+        #print(f._frontier[min(f._frontier)])
 
     except ValueError:
         print("Error. Not a valid input")
