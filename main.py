b4a123c4a5cd949b9b27f22d2497a7e43e1b9474
Samuel <samuel.gonzalez3@alu.uclm.es>
2018-10-26 17:46:57 +0200

    stress test additions
    max, min, avg times in stress test

--- main.py
+++ main.py
@@ -7,8 +7,12 @@ from treeNode import treeNode
 import time
 import sys
 
-"""def stressTest(f,p):
+def stressTest(f,p):
     elements = 0
+    avg = 0
+    maxim = 0
+    minim = 9999
+    
     try:
         while True:
             try:
@@ -17,16 +21,29 @@ import sys
                     node = treeNode(p._init_state, 0, "", elements)
                 else:
                     node = treeNode(newState, 0, "", elements)
+                start = time.time()
                 f.insert(node)
+                end = time.time()
                 elements+=1
-                #print(f._frontier[node._f]._state._md5)
+                timer = end - start
+                avg += timer
+                if(timer > maxim):
+                    maxim = timer
+                if(timer < minim):
+                    minim = timer
             except MemoryError:
                 print("full memory")
+                print('nº elements: '+elements)
+                print('min time: %.11f' % minim)
+                print('max time: %.11f' %  maxim)
+                print('avg time: %.11f' % (avg/elements))
                 break
     except KeyboardInterrupt:
-            print(min(f._frontier))
+            print('avg time: %.11f' % (avg/elements))
+            print('min time: %.11f' % minim)
+            print('max time: %.11f' %  maxim)
             return elements
-    return elements"""
+    return elements
 
 def main():
     try:
@@ -35,28 +52,28 @@ def main():
             raise ValueError
         print(filename + ".json") #print json file name
         p = problem('%s.json' % filename)
-        tn1 = treeNode(p._init_state, 0, "", 0)
-        tn2 = treeNode(p._init_state, 1, "", 1)
-        tn3 = treeNode(p._init_state, 2, "", 2)
+        #tn1 = treeNode(p._init_state, 0, "", 0)
+        #tn2 = treeNode(p._init_state, 1, "", 1)
+        #tn3 = treeNode(p._init_state, 2, "", 2)
         f = frontier()
-        f.insert(tn1)
-        f.insert(tn2)
-        f.insert(tn3)
+        #f.insert(tn1)
+        #f.insert(tn2)
+        #f.insert(tn3)
         #print(str(p._init_state))
         #print(f._frontier)
-        f.remove()
+        #f.remove()
         #print(f._frontier)
         #print(p._init_state._md5)
-        print(p._state_space._path)
-        print(p._state_space.belongNode(p._init_state))
-        p._state_space.successors(p._init_state)
-        print(p.isGoal(p._init_state))
+        #print(p._state_space._path)
+        #print(p._state_space.belongNode(p._init_state))    
+        #p._state_space.successors(p._init_state)
+        #print(p.isGoal(p._init_state))
 
-        """print('elements until memory error: ')
+        print('Waiting for memory error...or process abort (ctrl+c)')
         start = time.time()
-        print(stressTest(f,p))
-        print('time: %.11f' % (time.time() - start))
-        #print(f._frontier[min(f._frontier)])"""
+        print('nº elements: '+str(stressTest(f,p)))
+        print('total time: %.11f' % (time.time() - start))
+        #print(f._frontier[min(f._frontier)])
 
     except ValueError:
         print("Error. Not a valid input")
