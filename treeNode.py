8556da4dd92cf4787b4ff724cf3dd7c3ce28b1e2
jupcan <jpereac@gmail.com>
2018-11-14 10:19:56 +0100

    cost fix

--- treeNode.py
+++ treeNode.py
@@ -2,12 +2,13 @@ from state import state
 
 class treeNode():
 	def __init__(self, state, strategy, parent=None, cost=0, action=None, d=1):
-			self._parent = parent
 			self._state = state #current state of the problem
+			self._strategy = strategy
+			self._parent = parent
 			if parent is not None: self._cost = parent._cost+cost; self._d = parent._d+1
 			else: self._cost = cost; self._d = d
 			self._action = action
-			switch = {0: d, 1: -d, 2: -d, 3: -d, 4: cost, 5: cost}
+			switch = {0: d, 1: -d, 2: -d, 3: -d, 4: self._cost, 5: self._cost}
 			self._f = switch[strategy]
 
 	def getF(self):
