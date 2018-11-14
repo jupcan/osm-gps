458965951d2bdad915d3d89cad04495ee1107472
jupcan <jpereac@gmail.com>
2018-11-14 10:14:36 +0100

    d fix

--- treeNode.py
+++ treeNode.py
@@ -1,7 +1,7 @@
 from state import state
 
 class treeNode():
-	def __init__(self, state, strategy, parent=None, cost=1, action=None, d=0):
+	def __init__(self, state, strategy, parent=None, cost=0, action=None, d=1):
 			self._parent = parent
 			self._state = state #current state of the problem
 			if parent is not None: self._cost = parent._cost+cost; self._d = parent._d+1
